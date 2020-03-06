00316ca7d5481036eedac16010c6983a59a64736
jlidou <jlidou@isi-mtl.com>
2020-03-06 10:32:28 -0500

    refactoring folder

--- demos/demoJour2/Assets/Editor/GuiEditor.cs
+++ demos/demoJour2/Assets/Editor/GuiEditor.cs
@@ -7,15 +7,27 @@ using UnityEngine;
 [CustomEditor(typeof(GuiScript))]
 public class GuiEditor : Editor{
     private GuiScript guiScript;
+    private int Test;
+    private SerializedProperty TestSerialize;
     private void OnEnable() {
         guiScript = (GuiScript) target;
+        Test = guiScript.test;
+        TestSerialize = serializedObject.FindProperty("Test");
     }
     public override void OnInspectorGUI() {
         DrawDefaultInspector();
         EditorGUILayout.LabelField("Test");
+        // EditorGUILayout.PropertyField(TestSerialize);
+
+        serializedObject.ApplyModifiedProperties();
     }
 
    
 
 
+}
+
+[Serializable]
+class MonArme{
+    public int nbCoup;
 }
