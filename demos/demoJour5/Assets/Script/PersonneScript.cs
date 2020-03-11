971cabf3c1c1dde2f864fce2abed5e764d9c180e
jlidou <jlidou@isi-mtl.com>
2020-03-11 11:06:22 -0400

    add demo and exo

---
+++ demos/demoJour5/Assets/Script/PersonneScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonneScript : MonoBehaviour{
+    public PersonneScriptAbleObject myData;
+    private Renderer _renderer;
+    private void Awake() {
+        _renderer = gameObject.GetComponent<Renderer>();
+        _renderer.material.color = myData.couleurDeCheuveux;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ demos/demoJour5/Assets/Script/ScriptableObject/PersonneScriptAbleObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Personne", menuName = "Personne / Creation" )]
+public class PersonneScriptAbleObject : ScriptableObject{
+    public String nom;
+    public int age;
+    public Color couleurDeCheuveux;
+}
